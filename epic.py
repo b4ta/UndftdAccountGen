1f08a8c4b46467bd5c72e85d5aa224fde69b6ee2
bfourtaperiod <34697666+b4ta@users.noreply.github.com>
2019-05-10 02:47:00 -0400

    to-do list
    - made to-do list
    - please dont use my proxies

--- epic.py
+++ epic.py
@@ -1,36 +1,16 @@
 import requests, time, random, os, time
 import threading
-proxyList = [
-'204.188.230.33:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.34:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.35:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.36:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.37:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.38:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.39:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.40:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.41:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.42:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.43:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.44:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.45:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.46:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.47:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.48:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.49:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.50:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.51:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.52:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.53:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.54:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.55:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.56:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.57:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.58:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.59:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.60:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.61:3128:flashproxieschi:maymonthly83ni43',
-'204.188.230.62:3128:flashproxieschi:maymonthly83ni43']
+proxyList = ['insertProxiesHere']
+
+# To-Do List
+# - Get it to work lol
+# - Use proxies
+# - Rotate proxies when captcha is detected
+# - Be able to detect captchas with https://undefeated.com/challenge
+# - Make it create (for example) 2 separate accounts instead of making the same account twice when count = 2
+# - Simplify code
+# - Pull proxies from proxies.txt so my proxies aren't public
+# - 
 def start():
 	s = requests.session()
 	s.proxies = random.choice(proxyList)
