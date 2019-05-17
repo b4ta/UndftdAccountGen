33e62c6f6d0622994f743f4e8ef1ce5993019eba
bfourtaperiod <34697666+b4ta@users.noreply.github.com>
2019-05-17 01:25:38 -0400

    27 lines
    - epic

--- epic.py
+++ epic.py
@@ -11,15 +11,14 @@ def start():
 	print("Ok time to make " + count + " accounts with your weird domain name")
 	data = {"form_type": "create_customer",
 		"customer[email]": "email",
-		"customer[password]": "password"
+		"customer[password]": "password"}
 		# - There is a weird utf-8: checkmark thingy here but it makes the script wonky so I can't use it
-	       }
 	url = "https://undefeated.com/account/"
 	for i in range(int(count)):
 		user = username + "+" + str(random.randint(1,10000))
 		email = user + "@" + domain
-		request = s.post(url,json=data)
-		if request.status == 302:
+		create = s.post(url,json=data)
+		if create.status_code == 302:
 			print("Account: " + email + " made successfully.")
 			accounts = email + ":" + password
 			open("accounts.txt", "a").write("\n" + accounts)
