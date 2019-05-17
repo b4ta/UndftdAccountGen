b788b5d41356f629fe16403d7c339b1be299be18
bfourtaperiod <34697666+b4ta@users.noreply.github.com>
2019-05-17 01:11:34 -0400

    formatting changes
    - changed stuff
    - i make it look big nice

--- epic.py
+++ epic.py
@@ -1,13 +1,7 @@
-import requests, time, random, os, time
-import threading
-
+import requests, time, random, threading
 # To-Do List
-# - Get it to work lol
-# - Use proxies
-# - Rotate proxies when captcha is detected
-# - Be able to detect captchas with https://undefeated.com/challenge
-# - Simplify code
-# - Maybe use selenium, would be slower but could fix captcha stuff
+# - Detect captcha and use proxies to get around it
+# - What if I use selenium
 def start():
 	s = requests.session()
 	print("UNDFTD Account Gen Beta")
@@ -15,24 +9,22 @@ def start():
 	domain = input("Enter a catchall domain:")
 	count = input("Enter the number of accounts:")
 	password = input("Enter your password:")
-	print("Now generating " + count + " accounts under the " + domain + " domain.")
-	data = {
-		"form_type": "create_customer",
+	print("Ok time to make " + count + " accounts with your weird domain name.")
+	data = {"form_type": "create_customer",
 		"customer[email]": "email",
 		"customer[password]": "password"
-		}
+		# - There is a weird utf-8: checkmark thingy here but it makes the script wonky so I can't use it}
 	url = "https://undefeated.com/account/"
 	for i in range(int(count)):
-		random_username = username + "+" + str(random.randint(1,1000))
+		random_username = username + "+" + str(random.randint(1,10000))
 		email = random_username + "@" + domain
 		data["customer[email]"] = email
+		data["customer[password]"] = password
 		account_request = s.post(url,json=data)
-		if account_request.status_code == 302:
+		if account_request.status = 302:
 			print("Account: " + email + " made successfully.")
 			account_string = email + ":" + password
 			open("accounts.txt", "a").write("\n" + account_string)
 		else:
-			print("Failed to make account " + email)
-			print("Required Captcha (most likely). Wait for fix")
-			time.sleep(3)
+			print("Probably needed Captcha so haha stupid")
 start()
