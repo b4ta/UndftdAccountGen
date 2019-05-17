d4df7c1809074dd4357656e858ac2d3eb057e081
bfourtaperiod <34697666+b4ta@users.noreply.github.com>
2019-05-17 01:18:05 -0400

    other stuff
    - i made variables look un-dumb
    - i fixed data thing that i messed up

--- epic.py
+++ epic.py
@@ -1,5 +1,4 @@
 import requests, time, random, threading
-# To-Do List
 # - Detect captcha and use proxies to get around it
 # - What if I use selenium
 def start():
@@ -9,22 +8,21 @@ def start():
 	domain = input("Enter a catchall domain:")
 	count = input("Enter the number of accounts:")
 	password = input("Enter your password:")
-	print("Ok time to make " + count + " accounts with your weird domain name.")
+	print("Ok time to make " + count + " accounts with your weird domain name")
 	data = {"form_type": "create_customer",
 		"customer[email]": "email",
 		"customer[password]": "password"
-		# - There is a weird utf-8: checkmark thingy here but it makes the script wonky so I can't use it}
+		# - There is a weird utf-8: checkmark thingy here but it makes the script wonky so I can't use it
+	       }
 	url = "https://undefeated.com/account/"
 	for i in range(int(count)):
-		random_username = username + "+" + str(random.randint(1,10000))
-		email = random_username + "@" + domain
-		data["customer[email]"] = email
-		data["customer[password]"] = password
-		account_request = s.post(url,json=data)
-		if account_request.status == 302:
+		user = username + "+" + str(random.randint(1,10000))
+		email = user + "@" + domain
+		request = s.post(url,json=data)
+		if request.status == 302:
 			print("Account: " + email + " made successfully.")
-			account_string = email + ":" + password
-			open("accounts.txt", "a").write("\n" + account_string)
+			accounts = email + ":" + password
+			open("accounts.txt", "a").write("\n" + accounts)
 		else:
 			print("Probably needed Captcha so haha stupid")
 start()
